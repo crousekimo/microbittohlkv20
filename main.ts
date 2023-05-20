517e1a9fac58581b2726f9e52aa7d47de4e5ebca
crousekimo <43980978+crousekimo@users.noreply.github.com>
2023-05-20 14:06:33 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -62,7 +62,7 @@ let readserialdata='';
     //% blockId=returnserialdata1 block="read"
     //% weight=102
     export function returnserialdata1() {
-           let a = serial.readBuffer(1)
+           let a = serial.readBuffer(2)
            readserialdata = readserialdata + String.fromCharCode(a.getNumber(NumberFormat.UInt8LE, 0));
     }
     //% group="1.Setup"
