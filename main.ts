021ef9011b8a337b62200fdc3aff2cf62a5ec371
crousekimo <43980978+crousekimo@users.noreply.github.com>
2023-05-20 10:38:29 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,45 @@
         //% block="喚醒詞"
         wakeup_uni,
         //% block="退下"
-        exitUni
+        exitUni,
+        //% block="打開風扇"
+        openElectricfan,
+        //% block="關閉風扇"
+        closeElectricfan,
+        //% block="風速調到1檔"
+        AdjustGdarmin,
+        //% block="風速調到3檔"
+        AdjustGdarmax,
+        //% block="風速調到最大"
+        settiOPonehonor,
+        //% block="打開空調"
+        openkongtiao,
+        //% block="關閉空調"
+        closekongtiao,
+        //% block="升高溫度"
+        shenggaowendu,
+        //% block="降低溫度"
+        jiandiwendu,
+        //% block="打開檯燈"
+        turnon,
+        //% block="關閉檯燈"
+        turnoff,
+        //% block="開燈"
+        openled,
+        //% block="關燈"
+        closeled,
+        //% block="調到最亮"
+        setminld,
+        //% block="調到最暗"
+        turnoff,
+        //% block="亮一點"
+        zeogjialiangdu,
+        //% block="調暗一點"
+        jianxiaoliangdu,
+        //% block="燈紅色"
+        setcolorred
      }
-let check_word = ['wakeup_uni', 'exitUni'];
+let check_word = ['wakeup_uni', 'exitUni',"openElectricfan","closeElectricfan","AdjustGdarmin","AdjustGdarmax","settiOPonehonor","openkongtiao","closekongtiao","shenggaowendu",jiandiwendu",turnon",turnoff",openled",closeled",setminld",turnoff",zeogjialiangdu",jianxiaoliangdu",setcolorred"];
 namespace microbithlkv20 {
     //% group="1.Setup"  
     //% blockId=setMicrobit block="Initialize Microbit |TX %tx|RX %rx|Baud rate %baudrate "
