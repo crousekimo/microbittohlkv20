e964302c72a48e5f3841de38ec1027ff5627e4de
crousekimo <43980978+crousekimo@users.noreply.github.com>
2023-05-20 17:07:36 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -41,7 +41,7 @@
         //% block="燈紅色"
         setcolorred
      }
-let check_word = ['wakeup_uni', 'exitUni',"openElectricfan","closeElectricfan","AdjustGdarmin","AdjustGdarmax","settiOPonehonor","openkongtiao","closekongtiao","shenggaowendu","jiandiwendu","turnon","turnoff","openled","closeled","setminld","turnoff","zeogjialiangdu","jianxiaoliangdu","setcolorred"];
+let check_word = ['wakeup_uni', 'exitUni',"openElectricfan","closElectricfan","AdjustGdarmin","AdjustGdarmax","settiOPonehonor","openkongtiao","closekongtiao","shenggaowendu","jiandiwendu","turnon","turnoff","openled","closeled","setminld","turnoff","zeogjialiangdu","jianxiaoliangdu","setcolorred"];
 namespace microbithlkv20 {
 let readserialdata='';
     //% group="1.Setup"  
