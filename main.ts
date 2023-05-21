0abf3e37c8aa177d6ec083dead82ab58c5b37647
crousekimo <43980978+crousekimo@users.noreply.github.com>
2023-05-21 09:08:22 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -73,9 +73,17 @@
        //% block="一小時候關機"
        settiConehonor,
        //% block="一小時後開機"
-       settiOonehonor
+       settiOonehonor,
+       //% block="加大音量"
+       volumeUpUni,
+       //% block="減少音量"
+       volumeDownUni,
+       //% block="最大音量"
+       volumeMaxUni,
+       //% block="最小音量"
+       volumeMinUni
      }
-let check_word = ['wakeup_uni', 'exitUni',"openElectricfan","closElectricfan","openkongtiao","closekongtiao","shenggaowendu","jiandiwendu","turnon","turnoff","openled","closeled","setmaxld","setminld","zeogjialiangdu","jianxiaoliangdu","setcolorred","dakaiyuba","guanbiyuba","kdakainuanqi","kguanbinuanq","dakaicfeng","kguanbicfeng","dakaiyingsji","guanbiyingsji","kaishichus","tingzchus","dakaikaiguan","guanbikaiguan","dakaishaj","guanbishaj","dakaihonggan","guanbihonggan","dakaichuchou","gaunbichuchou","settiConehonor","settiOonehonor"];
+let check_word = ['wakeup_uni', 'exitUni',"openElectricfan","closElectricfan","openkongtiao","closekongtiao","shenggaowendu","jiandiwendu","turnon","turnoff","openled","closeled","setmaxld","setminld","zeogjialiangdu","jianxiaoliangdu","setcolorred","dakaiyuba","guanbiyuba","kdakainuanqi","kguanbinuanq","dakaicfeng","kguanbicfeng","dakaiyingsji","guanbiyingsji","kaishichus","tingzchus","dakaikaiguan","guanbikaiguan","dakaishaj","guanbishaj","dakaihonggan","guanbihonggan","dakaichuchou","gaunbichuchou","settiConehonor","settiOonehonor","volumeUpUni","volumeDownUni","volumeMaxUni","volumeMinUni"];
 namespace microbithlkv20 {
 let readserialdata='';
     //% group="1.Setup"  
